244d86eae1f56711d831400ad0c82ef2a48c08b3
Narenk-Git <knarendran561@gmail.com>
2020-10-29 17:38:06 +0530

    changed routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {Quiz1Component} from './quiz1/quiz1.component';
 import{HostComponent}from './host/host.component'
 
-const routes: Routes = [{ path: 'quiz1', component:Quiz1Component },
+const routes: Routes = [
+ { path: '', redirectTo:"/host" ,pathMatch:"full"},
+ { path: 'quiz1', component:Quiz1Component },
 { path: 'quiz2', component:Quiz1Component },
 { path: 'quiz3', component:Quiz1Component },
 { path: 'quiz4', component:Quiz1Component },
 { path: 'quiz5', component:Quiz1Component },
-{path:'host',component:HostComponent}];
+{path:'host',component:HostComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
